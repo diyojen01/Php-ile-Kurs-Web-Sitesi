264a747de97553b51c7a1e24b5dafbca6509376a
diyojen01 <stkeskin@yahoo.com>
2023-12-13 01:28:08 +0300

    Initial commit

---
+++ partials/_editor.php
@@ -0,0 +1,13 @@
+
+
+<!-- <script src="https://cdn.ckeditor.com/4.23.0-lts/standard/ckeditor.js"></script>
+
+<script>
+    CKEDITOR.replace('aciklama');
+</script> -->
+
+
+<script src="https://cdn.ckeditor.com/4.20.2/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace('aciklama');
+</script>
